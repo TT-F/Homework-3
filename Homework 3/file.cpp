44a81a0740d2fc710a4f9bd57f4d6f7ee8e32df3
TT-F <Steven@liuyis-macbook-pro.local>
2017-02-10 10:59:25 -0800

    XcodeVersionAdded

--- Homework 3/file.cpp
+++ Homework 3/file.cpp
@@ -108,21 +108,21 @@ void openAndRedisplay(const File* f)
 	cout << endl;
 }
 
-int main()
-{
-	File* files[4];
-	files[0] = new TextMsg("fromFred.txt");
-	// Videos have a name and running time
-	files[1] = new Video("goblin.mpg", 3780);
-	files[2] = new Picture("kitten.jpg");
-	files[3] = new Picture("baby.jpg");
-
-	for (int k = 0; k < 4; k++)
-		openAndRedisplay(files[k]);
-
-	// Clean up the files before exiting
-	cout << "Cleaning up." << endl;
-	for (int k = 0; k < 4; k++)
-		delete files[k];
-}
+//int main()
+//{
+//	File* files[4];
+//	files[0] = new TextMsg("fromFred.txt");
+//	// Videos have a name and running time
+//	files[1] = new Video("goblin.mpg", 3780);
+//	files[2] = new Picture("kitten.jpg");
+//	files[3] = new Picture("baby.jpg");
+//
+//	for (int k = 0; k < 4; k++)
+//		openAndRedisplay(files[k]);
+//
+//	// Clean up the files before exiting
+//	cout << "Cleaning up." << endl;
+//	for (int k = 0; k < 4; k++)
+//		delete files[k];
+//}
 
